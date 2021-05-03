bb7c858baf1f486a4daf5a3f40ab701881496635
MaartenQHuijser <maarten@huijser.nl>
2021-05-03 22:02:33 +0200

    Add color change on toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,19 @@ import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
     return (
-        <header className='header'>
+        <header className="header">
             <h1>{title}</h1>
-            <Button color='green' text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+            <Button
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd}
+            />
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+    title: 'Task Tracker',
 }
 
 Header.propTypes = {
